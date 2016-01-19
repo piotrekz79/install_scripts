6affeafd34c7d1da6461d5a6dd35e422dd42dd74
iplab <iplab@ICN.iplab>
2016-01-19 18:22:54 +0100

    initial commit

---
+++ install-wireshark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#installs wireshark in the most recent stable version
+#warning: if you just use sudo apt-get install wireshark on ubuntu 14.04 
+#it will install version 1.10 which does not have OpenFlow dissector
+
+sudo add-apt-repository ppa:wireshark-dev/stable
+sudo apt-get update
+sudo apt-get install wireshark -y
+
+#normally non-root cannot sniff, this is a fix (agree)
+sudo dpkg-reconfigure wireshark-common
+
+sudo adduser $USER wireshark 
+echo "logoff/logon may be needed"
+
