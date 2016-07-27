87d177f8d18bad6208fb62d9d4df7006a68563dd
piotrekz79 <piotrekz79@github.com>
2016-07-27 10:56:22 +0200

    added comment on what to do if the chrome stalls

--- install-chrome.sh
+++ install-chrome.sh
@@ -4,3 +4,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y
+
+echo !!!!!!!!!!!!!!!!!!!!
+echo If the browser does not start, check ownership of ~/.config folder
+echo should be owned by user, not root
